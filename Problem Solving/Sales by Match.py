e85e6c9f7b95945c8bb2d76b1e104ce5018742b5
João Victor Paes Barreto <joao.paes.oficial@gmail.com>
2021-03-31 20:28:46 -0300

    Add files via upload

---
+++ Problem Solving/Sales by Match.py
@@ -0,0 +1,27 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, arr):
+    counter = 0
+    new_arr = set(arr)
+    for i in new_arr:
+        counter += arr.count(i) // 2 
+    return counter
+
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
