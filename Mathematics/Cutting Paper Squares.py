49bcc4781a9cade37dee48cd7b0d78bc2dc6ad53
João Victor Paes Barreto <joao.paes.oficial@gmail.com>
2021-05-20 21:37:44 -0300

    Cutting Paper Squares.py

---
+++ Mathematics/Cutting Paper Squares.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def solve(n, m):
+    return n * m  - 1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    result = solve(n, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
