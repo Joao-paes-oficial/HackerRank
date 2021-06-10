dd37278dc4e390ba1b4710c5bbd89d2b36c88e9c
João Victor Paes Barreto <joao.paes.oficial@gmail.com>
2021-06-09 21:05:25 -0300

    Algorithms\SImple Array Sum.js

---
+++ JavaScript/Algorithms/Simple Array Sum.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function simpleArraySum(ar) {
+    return ar.reduce((total, numbers) => {
+        return total + numbers;
+    }, 0)
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arCount = parseInt(readLine().trim(), 10);
+
+    const ar = readLine().replace(/\s+$/g, '').split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    const result = simpleArraySum(ar);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
